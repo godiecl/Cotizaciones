2b9515c7ed4d97c5c597245c6db0d67cdf23a3c0
Diego Urrutia Astorga <durrutia@ucn.cl>
2017-12-20 10:24:32 -0300

    Added: Logger.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,37 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Cotizaciones.Models;
+﻿using System; 
+using System.Collections.Generic; 
+using System.Diagnostics; 
+using System.Linq; 
+using System.Threading.Tasks; 
+using Microsoft.Extensions.Logging; 
+using Microsoft.AspNetCore.Mvc; 
+using Cotizaciones.Models; 
 
-namespace Cotizaciones.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace Cotizaciones.Controllers {
+    public class HomeController:Controller {
+        
+        private readonly ILogger _logger; 
+
+        public HomeController(ILogger < HomeController > logger) {
+            _logger = logger;
+            _logger.LogDebug("Building Home Controller ...");
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+        public IActionResult Index() {
+            return View(); 
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+        public IActionResult About() {
+            ViewData["Message"] = "Your application description page."; 
 
-            return View();
+            return View(); 
         }
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Contact() {
+            ViewData["Message"] = "Your contact page."; 
+
+            return View(); 
+        }
+
+        public IActionResult Error() {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id??HttpContext.TraceIdentifier }); 
         }
     }
 }
